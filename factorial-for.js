659e3a040924717bfbc57956763ba3e1776d8080
jeisonOrtega <jeisonempleo1@gmail.com>
2023-03-23 11:06:14 -0300

    Logica de ejercicio #3

---
+++ factorial-for.js
@@ -0,0 +1,8 @@
+var numero = 10
+
+
+for(i=numero-1 ;i>=1;i--)
+numero = numero*i 
+
+
+console.log(numero)
---
+++ factorial-while.js
@@ -0,0 +1,9 @@
+var dato = 5;
+var resultado = dato;
+var i = 1;
+while(i < dato){
+   resultado = resultado * i ;
+   i++;
+}
+
+console.log(resultado)
